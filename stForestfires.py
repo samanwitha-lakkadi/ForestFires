56752d1de2b74376861a7493e039b6230a2f0454
samanwitha-lakkadi <94488788+samanwitha-lakkadi@users.noreply.github.com>
2022-01-24 18:22:22 -0500

    stForestfires.py

--- stForestfires.py
+++ stForestfires.py
@@ -39,16 +39,5 @@ We can see all the data here by pressing check button.
 """
 #TODO: add code to show the raw data table only when the checkbox is selected 
 
-if st.checkbox('Show the raw data')
-    data
-
-
-"""
-## Raw Data
-
-We can see all the data here by pressing check button.
-"""
-#TODO: add code to show the raw data table only when the checkbox is selected 
-
 if st.checkbox('Show the raw data'):
     data
