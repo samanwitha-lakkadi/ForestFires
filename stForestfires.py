d969fe4d7ead123efcc2a1bf5df387fca67051c1
samanwitha-lakkadi <94488788+samanwitha-lakkadi@users.noreply.github.com>
2022-01-24 19:06:13 -0500

    updated stForestfires.py

--- stForestfires.py
+++ stForestfires.py
@@ -1,8 +1,9 @@
 
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(layout="wide")
